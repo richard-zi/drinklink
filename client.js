8738cf9a84dcf56ac9af7793398c766393236953
Richard Zimmermann <richard-z@outlook.de>
2023-03-29 22:07:07 +0200

    edit client.js and index.html

--- client.js
+++ client.js
@@ -1,22 +1,56 @@
-const form = document.querySelector('form');
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
 
-form.addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const formData = new FormData(event.target);
-    const username = formData.get('username');
-    const password = formData.get('password');
-    const response = await fetch('/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-    });
-    const data = await response.json();
-    if (response.ok) {
-        console.log(data.message);
-        console.log(`Logged in as ${data.user.username}`);
-    } else {
-        console.error(data.error);
+    const username = document.getElementById('login-username').value;
+    const password = document.getElementById('login-password').value;
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            console.log(data);
+            alert(`Erfolgreiche Anmeldung! Willkommen ${data.user.username}`);
+        } else {
+            const error = await response.json();
+            console.error(error);
+            alert('Fehler bei der Anmeldung: ' + error.error);
+        }
+    } catch (err) {
+        console.error(err);
+        alert('Fehler bei der Anmeldung');
+    }
+});
+document.getElementById('register-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('register-username').value;
+    const password = document.getElementById('register-password').value;
+
+    try {
+        const response = await fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.status === 201) {
+            const data = await response.json();
+            console.log(data);
+            alert('Erfolgreiche Registrierung!');
+        } else {
+            const error = await response.json();
+            console.error(error);
+            alert('Fehler bei der Registrierung: ' + error.error);
+        }
+    } catch (err) {
+        console.error(err);
+        alert('Fehler bei der Registrierung');
     }
 });
