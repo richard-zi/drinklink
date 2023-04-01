6a69032b66c6448c204d0915a6e57ed053ee006e
Richard Zimmermann <richard-z@outlook.de>
2023-04-01 21:31:28 +0200

    test

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const cors = require('cors');
 // Lade Umgebungsvariablen aus der .env-Datei
 dotenv.config();
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
+
 // Erstelle eine Express-App und konfiguriere sie
 const app = express();
 app.use(bodyParser.json());
