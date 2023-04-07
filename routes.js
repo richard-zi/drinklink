7421f91d7c08968028c6b64b90b0a6edbe3e6cf6
Richard Zimmermann <richard-z@outlook.de>
2023-04-07 15:27:51 +0200

    del buchungsfunktion

--- routes.js
+++ routes.js
@@ -38,9 +38,7 @@ async function registerHandler(req, res) {
     },
   });
   console.log(`User registered successfully: ${username}`);
-  res
-    .status(StatusCodes.CREATED)
-    .json({ message: "User registered successfully." });
+  res.status(StatusCodes.CREATED).json({ message: "User registered successfully." });
 }
 
 async function loginHandler(req, res) {
@@ -58,67 +56,33 @@ async function loginHandler(req, res) {
 
   if (!user) {
     console.log(`Invalid login attempt: ${username}`);
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ error: "Invalid username or password." });
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid username or password." });
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
     console.log(`Invalid login attempt: ${username}`);
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ error: "Invalid username or password." });
+    return res.status(StatusCodes.UNAUTHORIZED).json({ error: "Invalid username or password." });
   }
 
   console.log(`Successful login: ${username}`);
   req.session.userId = user.id;
-  res.status(StatusCodes.OK).json({
-    message: "Login successful",
-    user: { id: user.id, username: user.username },
-  });
+  res
+    .status(StatusCodes.OK)
+    .json({
+      message: "Login successful",
+      user: { id: user.id, username: user.username },
+    });
 }
 
 function logoutHandler(req, res) {
   req.session.destroy((err) => {
     if (err) {
       console.error(err);
-      return res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ error: "Server error, please try again." });
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Server error, please try again." });
     }
     res.status(StatusCodes.OK).json({ message: "Logout successful" });
   });
-}
-
-async function bookHandler(req, res) {
-  const { date, time, people } = req.body;
-  const userId = req.session.userId;
-
-  const newBooking = await prisma.booking.create({
-    data: {
-      date: new Date(date),
-      time: new Date(time),
-      people: people,
-      userId: userId,
-    },
-  });
-
-  console.log(`Booking created successfully: ${newBooking.id}`);
-  res
-    .status(StatusCodes.CREATED)
-    .json({ message: "Booking created successfully.", booking: newBooking });
-}
-
-async function getUserBookingsHandler(req, res) {
-  const userId = req.session.userId;
-
-  const bookings = await prisma.booking.findMany({
-    where: { userId: userId },
-    orderBy: [{ date: "asc" }, { time: "asc" }],
-  });
-
-  res.status(StatusCodes.OK).json({ bookings });
 }
 
 // Füge die Routen mit asyncHandler hinzu
@@ -128,15 +92,11 @@ router.post("/logout", logoutHandler);
 router.get("/protected", isAuthenticated, (req, res) => {
   res.status(StatusCodes.OK).json({ message: "Protected route accessed" });
 });
-router.post("/book", isAuthenticated, asyncHandler(bookHandler));
-router.get("/bookings", isAuthenticated, asyncHandler(getUserBookingsHandler));
 
 // Error-Handling-Middleware
 router.use((err, req, res, next) => {
   console.error(err);
-  res
-    .status(StatusCodes.INTERNAL_SERVER_ERROR)
-    .json({ error: "Server error, please try again." });
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: "Server error, please try again." });
 });
 
 module.exports = router;
